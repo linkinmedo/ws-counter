dcd3983c13d106329a9626d25f26e4381514f483
Mohammed Shammout <fdsmohammed@gmail.com>
2018-09-10 04:38:49 +0300

    use vue-cookies

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import VueCookies from "vue-cookies";
 
 Vue.config.productionTip = false;
+Vue.use(VueCookies);
 
 new Vue({
   store,
